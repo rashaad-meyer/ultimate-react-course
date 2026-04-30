958d5850e4365a2eb94dd81dd6e7762468f77e28
rashaadmeyer <rashaad.meyer@gmail.com>
2026-04-30 13:43:42 +0200

    format

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -3,23 +3,23 @@ import ReactDOM from "react-dom/client";
 
 function App() {
   return (
-  <div>
-    <h1>Hello React!</h1>
-    <Pizza />
-    <Pizza />
-    <Pizza />
-  </div>
- );
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
 }
 
 function Pizza() {
-    return (
-        <div>
-            <img src='pizzas/spinaci.jpg' alt="Pizza Spinaci"/>
-            <h2>Pizza Spinaci</h2>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        </div>
-    )
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
